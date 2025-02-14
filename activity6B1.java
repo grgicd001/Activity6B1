e621408e0064c5b73b4e1c67514fd66e8c11d056
DannyG <daniel.grgic@ontariotechu.net>
2025-02-13 22:08:02 -0500

    Version 2 created

--- activity6B1.java
+++ activity6B1.java
@@ -1,5 +1,6 @@
 public class activity6B1{
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("This should be version 2");
     }
 }
