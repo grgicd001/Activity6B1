51c50b766f5eff5a29bc49a41cb11cbf98e28bcb
DannyG <daniel.grgic@ontariotechu.net>
2025-02-13 19:35:05 -0500

    Version 2

--- newFile.java
+++ newFile.java
@@ -1,5 +1,7 @@
 public class newFile {
     public static void main(String[] args){
         System.out.println("This is a new file");
+        System.out.println("This is a change");
+        System.out.println("This is the third change change");
     }
 }
