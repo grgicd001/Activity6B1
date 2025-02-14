7557f54fa9f66464362e1054a6d4cb4b84fea44c
DannyG <daniel.grgic@ontariotechu.net>
2025-02-13 22:26:26 -0500

    Updating activity6B1.java from the branch

--- activity6B1.java
+++ activity6B1.java
@@ -2,5 +2,6 @@ public class activity6B1{
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("This should be version 2");
+        System.out.println("Updating activity6b1.java in the branch");
     }
 }
